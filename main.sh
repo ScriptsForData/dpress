7f5a436d1418b643d9d276fa83230208bfcb3b33
StanDoge <StanlyDoge@gmail.com>
2022-06-28 22:56:44 -0600

    Main conditional working

--- main.sh
+++ main.sh
@@ -2,15 +2,16 @@
 # Script to decompress files for rar or zip entension using unzip and unrar tool.
 # Standoge 28-06-22
 
-RARFILES='^.+\.rar$/gm'
-ZIPFILES='^.+\.zip$/gm'
+RARFILES='^.+\.rar$'
+ZIPFILES='^.+\.zip$'
 
-dpress(){
+ if [[ $1 =~ $RARFILES ]]; then
 
- if [[ $1 =~ RARFILES ]]; then
 	unrar -x $1
- elif [[ $1 =~ ZIPFILES ]]; then
+
+ elif [[ $1 =~ $ZIPFILES ]]; then
+
 	unzip $1
+
 fi
 
-}
