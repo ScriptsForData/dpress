38ff48a6c39ca51c7e1192e602db61b3185f394f
StanDoge <StanlyDoge@gmail.com>
2022-06-30 17:49:39 -0600

    Insert case inside if conditional

--- dpress.sh
+++ dpress.sh
@@ -4,46 +4,47 @@
 
 RARFILES='^.+\.rar$'
 ZIPFILES='^.+\.zip$'
-				
 ARGUMENT='^-.$'
+option=''
 
-if [[  ]]; then
-  
+if [[ $1 =~ $ARGUMENT  ]]; then
+  $option=$1
+
+  case $option in
+    -l)
+       if [[ $1 =~ $rarfiles ]]; then
+
+        unrar l $1
+
+       elif [[ $1 =~ $zipfiles ]]; then
+
+        unzip -l $1
+
+       fi
+    ;;
+    -d)
+       if [[ $1 =~ $rarfiles ]]; then
+
+        unrar $1 ad $2
+
+       elif [[ $1 =~ $zipfiles ]]; then
+
+        unzip $1 -d $2
+
+       fi
+    ;;
+    *)
+       if [[ $1 =~ $rarfiles ]]; then
+
+        unrar x $1
+
+       elif [[ $1 =~ $zipfiles ]]; then
+
+        unzip $1
+
+       fi
+    ;;
+  esac
+
 fi
 
-case $1 in
-  -l)
-     if [[ $1 =~ $rarfiles ]]; then
-
-      unrar l $1
-
-     elif [[ $1 =~ $zipfiles ]]; then
-
-      unzip -l $1
-
-     fi
-  ;;
-  -d)
-     if [[ $1 =~ $rarfiles ]]; then
-
-      unrar $1 ad $2
-
-     elif [[ $1 =~ $zipfiles ]]; then
-
-      unzip $1 -d $2
-
-     fi
-  ;;
-  *)
-     if [[ $1 =~ $rarfiles ]]; then
-
-      unrar x $1
-
-     elif [[ $1 =~ $zipfiles ]]; then
-
-      unzip $1
-
-     fi
-
-  ;;
-esac
