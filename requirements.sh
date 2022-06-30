e08261a1f5bf0952fdc751aa32feb8830e541329
StanDoge <StanlyDoge@gmail.com>
2022-06-30 17:30:26 -0600

    Added mac option to install requirements easily

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # Script to install tools that are necessary to run dpress.sh script
 
-sudo apt install unzip unrar
+case $1 in
+  -debian|-ubuntu) sudo apt install unzip unrar
+  ;;
+  -arch) sudo pacman -S unzip unrar  
+  ;;
+  -mac) brew  install rar && brew install unzip
+  ;;
+  *) echo "NOTE: Select some argument to use a package manager and install the requirements"
+esac
+
 
 
