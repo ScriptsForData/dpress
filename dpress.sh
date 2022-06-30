c0bde75dca90fba705a5e6f47a732530dc3ad5a2
StanDoge <StanlyDoge@gmail.com>
2022-06-30 17:56:07 -0600

    Fix variable numbers

--- dpress.sh
+++ dpress.sh
@@ -9,42 +9,41 @@ option=''
 
 if [[ $1 =~ $ARGUMENT  ]]; then
   $option=$1
-
-  case $option in
-    -l)
-       if [[ $1 =~ $rarfiles ]]; then
-
-        unrar l $1
-
-       elif [[ $1 =~ $zipfiles ]]; then
-
-        unzip -l $1
-
-       fi
-    ;;
-    -d)
-       if [[ $1 =~ $rarfiles ]]; then
-
-        unrar $1 ad $2
-
-       elif [[ $1 =~ $zipfiles ]]; then
-
-        unzip $1 -d $2
-
-       fi
-    ;;
-    *)
-       if [[ $1 =~ $rarfiles ]]; then
-
-        unrar x $1
-
-       elif [[ $1 =~ $zipfiles ]]; then
-
-        unzip $1
-
-       fi
-    ;;
-  esac
-
 fi
+
+case $option in
+  -l)
+     if [[ $2 =~ $rarfiles ]]; then
+
+      unrar l $2
+
+     elif [[ $2 =~ $zipfiles ]]; then
+
+      unzip -l $2
+
+     fi
+  ;;
+  -d)
+     if [[ $2 =~ $rarfiles ]]; then
+
+      unrar $2 ad $3
+
+     elif [[ $2 =~ $zipfiles ]]; then
+
+      unzip $2 -d $3
+
+     fi
+  ;;
+  *)
+     if [[ $1 =~ $rarfiles ]]; then
+
+      unrar x $1
+
+     elif [[ $1 =~ $zipfiles ]]; then
+
+      unzip $1
+
+     fi
+  ;;
+esac
 
