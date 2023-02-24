11b05b9b74e72f05c63d659448ecd55417b65145
StanDoge <stanlydoge@gmail.com>
2023-02-23 21:15:09 -0600

    `feat:` flag -r

--- dpress.sh
+++ dpress.sh
@@ -19,24 +19,24 @@ ARGUMENT='^-.+$'
 # Outputs:
 #   Writes files that are beging decompressed to stdout.
 #######################################
-listFiles () {  
-       if [[ $1 =~ $RARFILES ]]; then
-
+listFiles () {
+    if [[ $1 =~ $RARFILES ]]; then
+        
         unrar l $1
-
-       elif [[ $1 =~ $ZIPFILES ]]; then
-
+        
+        elif [[ $1 =~ $ZIPFILES ]]; then
+        
         unzip -l $1
-
-       elif [[ $1 =~ $TARFILES ]]; then
-
-        tar -ztvf $1 
-
-       elif [[ $1 =~ $XZFILES ]]; then
-
-        tar tvf $1 
-
-       fi
+        
+        elif [[ $1 =~ $TARFILES ]]; then
+        
+        tar -ztvf $1
+        
+        elif [[ $1 =~ $XZFILES ]]; then
+        
+        tar tvf $1
+        
+    fi
 }
 
 #######################################
@@ -52,19 +52,19 @@ listFiles () {
 #   None
 #######################################
 decompressAnotherDir () {
-       if [[ $1 =~ $RARFILES ]]; then
-
+    if [[ $1 =~ $RARFILES ]]; then
+        
         unrar e $1 $2
-
-       elif [[ $1 =~ $ZIPFILES ]]; then
-
+        
+        elif [[ $1 =~ $ZIPFILES ]]; then
+        
         unzip $1 -d $2
-
-       elif [[ $1 =~ $TARFILES || $1 =~ $XZFILES ]]; then
-
+        
+        elif [[ $1 =~ $TARFILES || $1 =~ $XZFILES ]]; then
+        
         tar -xf $1 --directory $2
-
-       fi
+        
+    fi
 }
 
 #######################################
@@ -80,23 +80,23 @@ decompressAnotherDir () {
 #   None
 #######################################
 compressFiles () {
-       if [[ $1 =~ $RARFILES ]]; then
-
+    if [[ $1 =~ $RARFILES ]]; then
+        
         rar a -r $1 $2
-
-       elif [[ $1 =~ $ZIPFILES ]]; then
-
+        
+        elif [[ $1 =~ $ZIPFILES ]]; then
+        
         zip -r $1 $2
-
-       elif [[ $1 =~ $TARFILES ]]; then
-
+        
+        elif [[ $1 =~ $TARFILES ]]; then
+        
         tar -zcvf $1 $2
-
-       elif [[ $1 =~ $XZFILES ]]; then
-
+        
+        elif [[ $1 =~ $XZFILES ]]; then
+        
         tar cJvf $1 $2
-
-       fi
+        
+    fi
 }
 
 #######################################
@@ -112,60 +112,58 @@ compressFiles () {
 #   None
 #######################################
 decompress () {
-       if [[ $1 =~ $RARFILES ]]; then
-
+    if [[ $1 =~ $RARFILES ]]; then
+        
         unrar x $1 > /dev/null
-
-       elif [[ $1 =~ $ZIPFILES ]]; then
-
+        
+        elif [[ $1 =~ $ZIPFILES ]]; then
+        
         unzip $1 > /dev/null
-
-       elif [[ $1 =~ $TARFILES || $1 =~ $XZFILES ]]; then
-
+        
+        elif [[ $1 =~ $TARFILES || $1 =~ $XZFILES ]]; then
+        
         tar -xzvf $1 > /dev/null
-
-      else
+        
+    else
         echo "Note: You must select a compressed file"
-      fi
+    fi
 }
 
 
-if [[ $1 =~ $ARGUMENT  ]]; then
-  case "$1" in
-    -l)
-
-      listFiles $2 
-    ;;
-    -c)
-      if [[ $# -ne 3 ]]; then
-        echo "Expected argument were not given, you've to follow this syntax:
-        <dpress -d file.extension directory>"
-
+if [[ $1 =~ $ARGUMENT ]]; then
+    case "$1" in
+        -l)
+            listFiles $2
+        ;;
+        -c)
+            if [[ $# -ne 3 ]]; then
+                echo "Expected argument were not given, you've to follow this syntax:
+                <dpress -d file.extension directory>"
       else
         compressFiles $2 $3
       fi
-     ;;   
-
+     ;;
     -d)
       if [[ $# -ne 3 ]]; then
-        echo "Expected argument were not given, you've to follow this syntax:
-        <dpress -d file.extension directory>"
-
-      else
-        decompressAnotherDir $2 $3
-      fi
-    ;;
-
-    *) echo -e "Note: Select a valid argument\n " 
-       echo -e "-l: To list files before to decompress\n
--d: To put a thid argument at end with path for output\n
-none: To decompress on current directory" 
-    ;;
-
-  esac
-
-else 
-
-  decompress $1
-
+                echo "Expected argument were not given, you've to follow this syntax:
+                <dpress -d file.extension directory>"
+                
+            else
+                decompressAnotherDir $2 $3
+                rm -rf $2
+            fi
+        ;;
+        
+        *) echo -e "Note: Select a valid argument\n "
+            echo -e "-l: To list files before to decompress\n
+           -d: To put a thid argument at end with path for output\n
+            none: To decompress on current directory"
+        ;;
+        
+    esac
+else
+    
+    decompress $1
+    rm -r $1
+    
 fi
