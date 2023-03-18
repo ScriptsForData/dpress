2542550cb7c6ae253a4071ddb0f093b804562137
StanDoge <stanlydoge@gmail.com>
2023-03-17 21:18:16 -0600

    `chore:` project boilerplate

---
+++ src/list_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/list_command.sh'"
+echo "# code for 'dpress list' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
