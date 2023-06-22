f58d0fa91f35ef25a6f87fe2f2a5e6b7cba222f9
standoge <stanlydoge@gmail.com>
2023-06-22 16:40:03 -0600

    just keep it simple
    remove automatation of installing by uname -r command, instead just select the os using parameters

--- requirements.sh
+++ requirements.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 # Script to install tools that are necessary to run dpress.sh script
 
-OS=$(uname -r)
-ARCHREG='^.+arch.+$'
-UBUNTUREG='^.+ubuntu.+$'
-DEBIANREG='^.+debian.+$'
-WSL='(\d+\.)+\d+-[a-z]+-[a-z]+-[A-Z]+\d'
-
-if [[ $OS =~ $ARCHREG ]]; then
-    sudo pacman -S unzip unrar xz
-    elif [[ $OS =~ $UBUNTUREG || $OS =~ $DEBIANREG || $OS =~ $WSL ]]; then
-    sudo apt install unzip unrar xz
-fi
-
-
-# ╭──────────────────────────────────────────────────────────╮
-# │ case $1 in                                               │
-# │   -debian|-ubuntu) sudo apt install unzip unrar          │
-# │   ;;                                                     │
-# │   -arch) sudo pacman -S unzip unrar                      │
-# │   ;;                                                     │
-# │   -mac) brew  install rar && brew install unzip          │
-# │   ;;                                                     │
-# │   *) echo "NOTE: Select some argument to use a           │
-# │  package manager and install the requirements"           │
-# │ esac                                                     │
-# ╰──────────────────────────────────────────────────────────╯
+case $1 in
+    -debian|-ubuntu) sudo apt install unzip unrar
+    ;;
+    -arch) sudo pacman -S unzip unrar
+    ;;
+    -mac) brew  install rar && brew install unzip
+    ;;
+    *) echo "NOTE: Select some argument to use a
+        package manager and install the requirements"
+esac
 
 
 
